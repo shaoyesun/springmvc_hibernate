69df5bfc7fcb86b7b93e77d795af45bc680fa8b3
git <sunjf@biomarker.com.cn>
2017-06-01 16:23:23 +0800

    shiro

--- src/main/java/example/controller/shiro/LoginController.java
+++ src/main/java/example/controller/shiro/LoginController.java
@@ -5,9 +5,7 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import example.entity.shiro.User;
